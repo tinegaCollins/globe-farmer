eac40c627e51f2cc208d3303505117ab88ddd09e
tinegaCollins <collinstinega003@gmail.com>
2022-02-17 16:53:32 -0800

    new commit

--- Front-end/script.js
+++ Front-end/script.js
@@ -3,3 +3,11 @@ const navBar = document.querySelector("#menu1");
 menuButton.addEventListener("click",()=>{
     navBar.classList.toggle("moveRight")
 })
+
+//log in  
+const login = document.querySelector("#login");
+login.addEventListener("click",()=>{
+    window.open("login.html", '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
+})
+
+//sign up on log in page 
