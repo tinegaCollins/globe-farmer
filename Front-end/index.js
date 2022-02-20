fcc69bb6b1dc064422c86a9b029348fad5a17e58
tinegaCollins <collinstinega003@gmail.com>
2022-02-20 09:10:56 -0800

    new commit

--- Front-end/index.js
+++ Front-end/index.js
@@ -5,3 +5,16 @@ const side = document.querySelector("#sideBar")
 navBar.addEventListener("click",()=>{
     side.classList.toggle("moveSidebar")
 })
+
+
+const popular = document.querySelector("#popular")
+const items = document.querySelector("#popularItems")
+const buy = document.querySelector(".buy")
+items.addEventListener("click",(e)=>{
+    if(e.target.className = "button1"){
+        items.style.display = "none"
+        buy.style.display = "flex"
+        popular.innerHTML = ""
+    }
+    
+})
