4e83dfb91ee80c66dda95643ab3ddf56130594e0
tinegaCollins <collinstinega003@gmail.com>
2022-02-20 11:37:40 -0800

    new commit

--- Front-end/index.js
+++ Front-end/index.js
@@ -7,14 +7,57 @@ navBar.addEventListener("click",()=>{
 })
 
 
-const popular = document.querySelector("#popular")
-const items = document.querySelector("#popularItems")
-const buy = document.querySelector(".buy")
-items.addEventListener("click",(e)=>{
-    if(e.target.className = "button1"){
-        items.style.display = "none"
+//buttons 
+
+const buttons = document.querySelectorAll(".button1");
+const buttonsArray = Array.from(buttons);
+for(let i=0; i < buttonsArray.length; i++){
+    let k = i + 1;
+    buttonsArray[i].addEventListener("click",()=>{
+        let popularItems = document.querySelector("#popularItems")
+        popularItems.style.display = "none"
+        let buy = document.querySelector(".buy")
         buy.style.display = "flex"
-        popular.innerHTML = ""
+        let h2  = document.querySelector("#popular")
+        h2.innerHTML = ""
+        // console.log(`button no ${k}`)
+    });
+}
+
+//x button 
+ const xbutton = document.querySelector(".fa-xmark")
+xbutton.addEventListener("click",()=>{
+    let popularItems = document.querySelector("#popularItems")
+        popularItems.style.display = "flex"
+        let buy = document.querySelector(".buy")
+        buy.style.display = "none"
+        let h2  = document.querySelector("#popular")
+        h2.innerHTML = "Popular Items"
+})
+
+
+//add and minus value on checklist
+
+const minus = document.querySelector(".fa-minus")
+const add = document.querySelector(".fa-plus")
+minus.addEventListener("click",()=>{
+    let q = document.querySelector("#addQuantity")
+    let num = parseInt(q.value)
+    if (num>=0){
+        num--
     }
+    q.value = num
+})
+
+add.addEventListener("click",()=>{
+    let q = document.querySelector("#addQuantity")
+    let num = parseInt(q.value)
+        num++
+    q.value = num
+})
+
+//add to cart 
+const cart = document.querySelector(".cta")
+cart.addEventListener("click",()=>{
     
 })
--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const menuButton = document.querySelector("#menu")
 const navBar = document.querySelector("#menu1");
 menuButton.addEventListener("click",()=>{
     navBar.classList.toggle("moveRight")
+    const icon  = document.querySelector("#menu i")
+    icon.classList.toggle("fa-bars")
+    icon.classList.toggle("fa-xmark")
 })
 
 //log in  
