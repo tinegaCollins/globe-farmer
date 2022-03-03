9612d55e3dbf082238af48c5df0d0b85ac7c9ff7
tinegaCollins <collinstinega003@gmail.com>
2022-03-03 12:51:19 +0300

    new commit

---
+++ Front-end/farmersChoice/script.js
@@ -0,0 +1,4 @@
+const login = document.querySelector("#SignInBtn")
+login.addEventListener("click",()=>{
+    window.open("../login.html", '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
+})
