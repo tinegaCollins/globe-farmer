c68d533202318c60c5075cd8dc00c2672756e72b
tinegaCollins <collinstinega003@gmail.com>
2022-03-29 00:08:24 +0300

    new commit

---
+++ Front-end/post-stuff/script.js
@@ -0,0 +1,15 @@
+var newCustomer ={
+    id : 12,
+    name : "lil baby",
+    location: "mombasa",
+    phone : +254791729544,
+    products : [
+        {
+            item : "onions",
+            price: "200 per kg",
+        },
+        //places to add new items
+    ]
+}
+
+module.exports = newCustomer
