29aff58ebe790f1b0d3353cdec4465204258bd51
tinegaCollins <collinstinega003@gmail.com>
2022-04-18 23:54:33 +0300

    final commit today

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-   <h1>newwwwwww</h1>
+    <topBar/>
 </template>
 
+<script>
+
+import topBar from './components/top-bar.vue'
+
+//import css for this  for assets folder
+import './assets/styles.css'
+export default {
+  components :{
+    topBar
+  }
+
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/top-bar.vue
@@ -0,0 +1,70 @@
+<template>
+   <div class="content">
+      <img src="../assets/logo.png" alt="company logo">
+      <div class="details">
+         <div class="user" @click="showUser">
+            <img src="../assets/user-solid.svg" alt="user">
+            <span v-if="showUserData">{{user}}</span>
+         </div> 
+         <div class="cart" @click="showCart">
+            <img src="../assets/cart.svg" alt="cart">
+            <span v-if="showCartData">{{cart}}</span>
+         </div>
+      </div>
+   </div>
+   <input type="text" placeholder="search for items/farmer">
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+         showUserData: false,
+         showCartData: false,
+         user: 'User',
+         cart: 'Cart'
+      }
+   },
+   methods: {
+      showUser() {
+         this.showUserData = !this.showUserData
+      },
+      showCart() {
+         this.showCartData = !this.showCartData
+      }
+   }
+
+}
+</script>
+
+<style scoped>
+    .content{
+       height: 30%;
+       display: flex;
+       margin-top: 20px;
+    }
+    img{
+       height: 25px;
+       width: auto;
+    }
+    .content > img {
+      height: 70px;
+      width: auto;
+      margin: 0 auto 0 20px;
+
+    }
+    .details{
+       display: flex;
+       align-items: center;
+       width: 100px;
+       justify-content: space-around;
+    }
+    input{
+       width: 300px;
+       height: 30px;
+       border-radius: 5px;
+       border: 1px solid #ccc;
+       padding: 0 10px;
+       margin: 0 auto 0 auto;
+    }
+</style>
