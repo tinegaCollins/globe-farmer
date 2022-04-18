756c516507571649799bf7207556c7c5e64c1d17
tinegaCollins <collinstinega003@gmail.com>
2022-04-18 22:21:52 +0300

    new commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+   <h1>newwwwwww</h1>
 </template>
 
 <style>
