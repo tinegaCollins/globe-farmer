979030d8ad33f6d994830108a2799d76fa45ecca
tinegaCollins <collinstinega003@gmail.com>
2022-05-01 14:23:07 +0300

    chat box on the way

--- src/pages/messages/messages-tab.vue
+++ src/pages/messages/messages-tab.vue
@@ -1,13 +1,69 @@
 <template>
-  <h1> this is the message tab </h1>
+  <div class="wrapper">
+    <appTopBar/>
+    <main>
+      <h3>messages</h3>
+      <div class="maincontent">
+        <div class="notifications-tab">
+
+        </div>
+        <div class="chat-box"></div>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-export default {
 
+import appTopBar from "../../components/appTopBar.vue"
+import '../../assets/styles/global.css'
+export default {
+  components: {
+    appTopBar
+  },
+  data(){
+    return{
+      messages: [
+        {type: "clement kipkirui", body: "Lorem ipsum dolor sit amet consectetur."},
+        {type: "me", body: "Lorem, ipsum dolor."},
+        {type: "other", body: "Lorem, ipsum dolor. to us"},
+        {type: "me", body: "okay"},
+      ]
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+main{
+  font-family: var(--main-font);
+  padding: 20px;
+}
+main > h3{
+  font-family: var(--main-font);
+  position: relative;
+}
+main > h3::after{
+  content: "";
+  width: 85px;
+  height: 2px;
+  background-color: black;
+  position: absolute;
+  left: 0;
+  bottom: -5px;
+}
+.maincontent{
+  display: flex;
+  flex-wrap: nowrap;
+  padding: 30px;
+  gap: 20px;
+}
+.notifications-tab, .chat-box{
+  height: calc(100vh - 180px);
+  width: 100%;
+  background-color: red;
+}
+.chat-box{
+  background-color: yellow;
+}
 </style>
